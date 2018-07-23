eb6f1d9cdb34dfd6842e03ab7c5b4010db38af1c
sawannrl <sawan.kumar@aurigait.com>
2018-07-23 17:01:46 +0530

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,29 @@
+window.onload = function() {
+    var jsonData;
+    readTextFile("data.json", function(data) {
+        jsonData = JSON.parse(data);
+        jsonData.forEach((element, index) => {
+            console.log('In Read File', element);
+            var html = '<p>' + element.name + '</p><p>' + element.age + '</p><p>' + element.sex + '</p>';
+            document.getElementById('card' + (index + 1)).innerHTML = html;
+        });
+    });
+};
+
+function readTextFile(file, callback) {
+
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4) {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
+
+
 function openMenu(elm) {
     console.log();
     elm.parentNode.classList.toggle("open");
