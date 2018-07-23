b921e5304399bbd652ca5c46555af59693228285
sawannrl <sawan.kumar@aurigait.com>
2018-07-23 23:23:36 +0530

    Final Commit

--- js/script.js
+++ js/script.js
@@ -3,8 +3,7 @@ window.onload = function() {
     readTextFile("data.json", function(data) {
         jsonData = JSON.parse(data);
         jsonData.forEach((element, index) => {
-            console.log('In Read File', element);
-            var html = '<p>' + element.name + '</p><p>' + element.age + '</p><p>' + element.sex + '</p>';
+            var html = element.name + ', ' + element.age + ', ' + element.sex;
             document.getElementById('card' + (index + 1)).innerHTML = html;
         });
     });
@@ -25,7 +24,6 @@ function readTextFile(file, callback) {
 
 
 function openMenu(elm) {
-    console.log();
     elm.parentNode.classList.toggle("open");
 }
 
@@ -33,12 +31,38 @@ function allowDrop(ev) {
     ev.preventDefault();
 }
 
+var tempId = '';
+
 function drag(ev) {
     ev.dataTransfer.setData("data", ev.target.id);
+    tempId = document.getElementById(ev.target.id).parentNode.id;
 }
 
-function drop(ev, elm) {
+function drop(ev) {
+    console.log(tempId);
     ev.preventDefault();
     var data = ev.dataTransfer.getData("data");
     ev.target.appendChild(document.getElementById(data));
+
+    setTimeout(function() {
+        var elmFirst = document.getElementById('outer_card1').getElementsByClassName("dragable");
+        var elmSecond = document.getElementById('outer_card3').getElementsByClassName("dragable");
+        var text1 = '',
+            text2 = '';
+        for (var i = 0; i < elmFirst.length; i++) {
+            text1 = text1 + elmFirst[i].innerHTML;
+        }
+        for (var j = 0; j < elmSecond.length; j++) {
+            text2 = text2 + elmSecond[j].innerHTML;
+        }
+        setTimeout(function() {
+            if (text1 == text2) {
+                var temp = document.getElementById(data);
+                ev.target.removeChild(document.getElementById(data));
+                document.getElementById(tempId).appendChild(temp);
+                alert('Card 1 content and card 3 content could not be same');
+            }
+        }, 100);
+    }, 100);
+
 }
